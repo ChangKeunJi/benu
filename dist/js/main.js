d5b6c23182af7656223471ff5657f7ed96d85517
HellooCK <46767604+HellooCK@users.noreply.github.com>
2019-09-07 14:34:08 +1000

    about page

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,8 +3,12 @@ const menuNav = document.querySelector(".menu-nav");
 const navItem = document.querySelectorAll(".nav-item");
 const navLink = document.querySelectorAll(".nav-link");
 const brand = document.getElementById("brand");
+const header = document.querySelector("header");
+const body = document.querySelector("body");
 
 let showMenu = false;
+
+// Hamburger Menu
 
 const toggle = () => {
   if (!showMenu) {
@@ -20,26 +24,52 @@ const toggle = () => {
   }
 };
 
+menuBtn.addEventListener("click", toggle);
+
+//! Hover - Menu Border bottom effect
+
 const borderBottom = function() {
   navLink.forEach(link => link.classList.remove("active"));
 
   this.classList.add("active");
 };
 
-// Hamburget Menu
-
-menuBtn.addEventListener("click", toggle);
-
-// Hover - Border bottom
-
 navLink.forEach(nav => {
   nav.addEventListener("mouseover", borderBottom);
 });
 
-// Home - Text effect
+// Close menu when click outside of menu.
 
-const showText = () => {
-  brand.classList.add("text-show");
+// const closingMenu = e => {
+//   if (e.target !== menuBtn && e.target !== navLink) {
+//     menuBtn.classList.remove("close");
+//     menuNav.classList.remove("show");
+//     navItem.forEach(i => i.classList.remove("show"));
+//     showMenu = false;
+//   }
+// };
+
+// body.addEventListener("click", closingMenu);
+
+// Image Gallery
+
+const imageGallery = () => {
+  const selected = document.querySelector(".img-selected");
+  const previews = document.querySelectorAll(".img-preview img");
+
+  const showImg = e => {
+    previews.forEach(preview => preview.classList.remove("img-active"));
+
+    const selectedSrc = e.target.src;
+
+    selected.src = selectedSrc;
+
+    e.target.classList.add("img-active");
+  };
+
+  previews.forEach(preview => {
+    preview.addEventListener("click", showImg);
+  });
 };
 
-setTimeout(showText, 500);
+imageGallery();
